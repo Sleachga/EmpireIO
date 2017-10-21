134721e3964288b0baf8f2820d8658d7e6f5662a
Sleachga <leach.sandy3@gmail.com>
2017-10-21 13:09:55 -0400

    Changed serve.listen to work with Heroku

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/',function(req, res)
 // Tells server where to find needed files
 app.use('/client',express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
 // server.js
